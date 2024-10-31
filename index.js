0f33dd5518091a80c94818ad53533a132355463a
khalil NOUI <noui.khalil.pr@gmail.com>
2024-10-31 16:08:56 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#CCFF00"
+
+const vertical = parseInt(dodger.style.bottom.replace('px', ''), 10)
+const horizontal = parseInt(dodger.style.left.replace('px', ''), 10)
+
+function moveDodgerRight() {
+    horizontal < 360 ? dodger.style.left = `${horizontal + 10}px`: console.log("no right")}
+    
+function moveDodgerLeft() {
+    horizontal > 0 ? dodger.style.left =  `${horizontal - 10}px`: console.log("no left");}
+            
+
+document.addEventListener("keydown", (e) => {
+    moveDodgerLeft(e)
+    moveDodgerRight(e)
+})
+
+
+    
+
+
+
+
+
